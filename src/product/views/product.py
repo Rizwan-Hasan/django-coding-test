9329eb6fdb4c5178209b02c6683e52f65c8969de
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2021-12-25 21:08:13 +0600

    done

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,14 +1,26 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Product, Variant
 
 
 class CreateProductView(generic.TemplateView):
-    template_name = 'products/create.html'
+    template_name = "products/create.html"
 
     def get_context_data(self, **kwargs):
         context = super(CreateProductView, self).get_context_data(**kwargs)
-        variants = Variant.objects.filter(active=True).values('id', 'title')
-        context['product'] = True
-        context['variants'] = list(variants.all())
+        variants = Variant.objects.filter(active=True).values("id", "title")
+        context["product"] = True
+        context["variants"] = list(variants.all())
+        return context
+
+
+class ListProductView(generic.TemplateView):
+    template_name = "products/list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ListProductView, self).get_context_data(**kwargs)
+        products = Product.objects
+        context["product"] = True
+        context["products"] = list(products.all())
+        print(products.all())
         return context
